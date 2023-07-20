79e67ff6b8d2b06c51c85077cb2cd688e0ebeff1
2020311920 <80453145+2020311920@users.noreply.github.com>
2023-07-20 11:56:46 +0900

    강성철이 Update HellorWord.cpp

--- src/HellorWord.cpp
+++ src/HellorWord.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
-int main(){
-    cout<<"Hello World";
+int sumFromZero(int num){
+    int sum=0;
+    for(int i=0;i<=num;i++) sum+=i;
+    return sum;
+}
 
+int main(){
+    cout<<"Hello World"<<"\n";
+
+    int num = 10;
+    cout<<sumFromZero(num);
+    
     //I don't know why I feel so nervous when doing this stuff....god plz
 }

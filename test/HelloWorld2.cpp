46e6e6c5f79adf20a182d0556ab8d2a968776df3
2020311920 <80453145+2020311920@users.noreply.github.com>
2023-07-20 11:58:58 +0900

    강성철이 Update HelloWorld2.cpp

--- test/HelloWorld2.cpp
+++ test/HelloWorld2.cpp
@@ -3,7 +3,13 @@
 using namespace std;
 
 int main(){
-    cout<<"Hello World";
+    cout<<"Hello World"<<"\n";
+    int score = 98;
+    while(score!=101){
+        cout<<score++<<"\n";
+    }
     cout<<"Actually I don't want to say hello";
-    //I don't know why I feel so nervous when doing this stuff....god plz
+
+    
+    
 }

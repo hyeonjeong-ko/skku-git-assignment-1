a6b4293d703b19db6c2e462682c3302e760b67f1
2020311920 <windalike12@gmail.com>
2023-07-20 10:58:40 +0900

    강성철이 vscode를 활용하여 src와test를 업로드합니다

---
+++ test/HelloWorld2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World";
+    cout<<"Actually I don't want to say hello";
+    //I don't know why I feel so nervous when doing this stuff....god plz
+}

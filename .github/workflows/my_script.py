64a1ec441a15eccce1d46967e32ca8179f7b40a9
hyeonjeong-ko <72601276+hyeonjeong-ko@users.noreply.github.com>
2023-08-17 02:43:17 +0900

    Update my_script.py

--- .github/workflows/my_script.py
+++ .github/workflows/my_script.py
@@ -1,7 +1,19 @@
 import os
 
-# Get the value of the environment variable
-my_env_var = os.environ.get('MY_ENV_VAR')
+# Get user information from environment variables
+user_name = os.environ.get('USER_NAME')
+commit_time = os.environ.get('COMMIT_TIME')
+commit_message = os.environ.get('COMMIT_MESSAGE')
 
-# Use the environment variable value
-print("Value of MY_ENV_VAR:", my_env_var)
+# Print the user information
+print("User Name:", user_name)
+print("Commit Time:", commit_time)
+print("Commit Message:", commit_message)
+
+# import os
+
+# # Get the value of the environment variable
+# my_env_var = os.environ.get('MY_ENV_VAR')
+
+# # Use the environment variable value
+# print("Value of MY_ENV_VAR:", my_env_var)
